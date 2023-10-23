b0a9872dffaca62ca47d90ac10296371ed1cfa18
NordAl89 <alancantry@gmail.com>
2023-10-23 14:49:01 +0300

    adoptation header 375

---
+++ front/src/blocks/header/header.js
@@ -0,0 +1,23 @@
+o2.header = {
+	setPlaceholder() {
+	  const inputElement = document.querySelector('._inputHeader');
+	  if (window.innerWidth > 768) {
+		inputElement.placeholder = 'Автор, название, ISBN';
+	  } else if (window.innerWidth <= 768) {
+		inputElement.placeholder = 'Что будем искать?';
+	  }
+	},
+  
+	init() {
+	  this.setPlaceholder(); // Set the initial placeholder value
+  
+	  // Add an event listener for window resize
+	  window.addEventListener('resize', () => {
+		this.setPlaceholder(); // Update the placeholder value on window resize
+	  });
+	}
+  };
+  
+  // Initialize the header functionality
+  o2.header.init();
+  

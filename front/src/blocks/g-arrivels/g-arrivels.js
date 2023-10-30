6280788e7cf8040053f2da1a6c5657186837a666
NordAl89 <alancantry@gmail.com>
2023-10-30 17:27:28 +0300

    subscribe

--- front/src/blocks/g-arrivels/g-arrivels.js
+++ front/src/blocks/g-arrivels/g-arrivels.js
@@ -7,7 +7,27 @@ o2.gArrivels =
 			{
 				title.textContent = 'Новые поступления'
 			}
+	},
+
+	changePrice()
+	{		
+		const price = document.querySelector('._article-arrivels__price')
+		// console.log(price.textContent)
+		if(price === null){
+			return
+		}
+
+		if(window.innerWidth <= 768)
+		{
+			price.textContent = '4 450 ₽ '
+		}else {
+			price.textContent = '450 ₽'
+		}
+
 	}
+
+
 	
 }
 o2.gArrivels.blockName()
+o2.gArrivels.changePrice()
